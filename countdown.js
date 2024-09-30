591128d4e0b4e036bb95e835e7430032bc1c604f
Cesar <cesarnsingi@hotmail.com>
2024-10-01 03:31:31 +0400

    display result

--- countdown.js
+++ countdown.js
@@ -15,4 +15,6 @@ let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+document.getElementById("countdown").innerHTML = days + "d" + hours + "h " + minutes + "m " + seconds + "s"; // Display the result 
+
 })
