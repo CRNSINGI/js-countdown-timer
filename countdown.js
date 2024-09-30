6c0e6f064a776729ea38fd4ff9360744b393d95a
Cesar <cesarnsingi@hotmail.com>
2024-10-01 03:14:17 +0400

    update and get date

--- countdown.js
+++ countdown.js
@@ -1,1 +1,12 @@
 let countDownDate = new Date("Dec 31, 2024 23:59:59").getTime(); // Set the date counting down
+
+//Update the count down every 1 second 
+
+let x = setInterval(function() {
+
+let now = new Date().getTime(); // Get todays date and time 
+
+
+
+
+})
