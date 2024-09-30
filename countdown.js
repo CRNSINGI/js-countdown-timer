132f8bce4f677a661f201387d160caebdb11b7b1
Cesar <cesarnsingi@hotmail.com>
2024-10-01 03:33:43 +0400

    if the count is over

--- countdown.js
+++ countdown.js
@@ -15,6 +15,11 @@ let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-document.getElementById("countdown").innerHTML = days + "d" + hours + "h " + minutes + "m " + seconds + "s"; // Display the result 
+document.getElementById("countdown").innerHTML = days + "d" + hours + "h " + minutes + "m " + seconds + "s"; // Display the result
 
-})
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+}
+
+},1000);
